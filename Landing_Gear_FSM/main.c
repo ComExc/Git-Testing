15285549311ba4419d04151a454bcece5bf9b729
Rahul Mondal <rahulnsjg@gmail.com>
2022-07-10 12:13:50 +0530

    comments added in main.c

--- Landing_Gear_FSM/main.c
+++ Landing_Gear_FSM/main.c
@@ -8,12 +8,12 @@
 #include "States.h"
 
 static void (*state_table[])(void) = {Gear_Down, Checking_Before_Takeoff, Raising_Gear,
-									Gear_Up, Checking_Before_Landing, Lowering_Gear};
+									Gear_Up, Checking_Before_Landing, Lowering_Gear};//Declaration of array of Function pointer
 int main()
 {
 	Init_State_Machine();
 	while(1)
 	{
-		state_table[current_state]();
+		state_table[current_state]();//Calling the states 
 	}
 }
